a02d9b51b876ca9d9285fdd36350b06bd1c35d94
JMH <jon@simplyagree.net>
2016-04-03 16:20:10 -0500

    Registering models in admin

--- terms/admin.py
+++ terms/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Term, Pronunciation
+
+# for future modification to admin
+
+# class TermAdmin(admin.ModelAdmin):
+#     model = Term
+#
+#
+# class PronunciationAdmin(admin.ModelAdmin):
+#     model = Pronunciation
+
+
+admin.site.register(Term)
+admin.site.register(Pronunciation)
