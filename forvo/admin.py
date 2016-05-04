f685418965ccd1c7d2d5e628fe9753d8068b478d
JMH <jon@simplyagree.net>
2016-05-04 18:47:57 -0500

    Integration of Forvo models into add term form

--- forvo/admin.py
+++ forvo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ForvoWord, ForvoPronunciation
+
+
+admin.site.register(ForvoWord)
+admin.site.register(ForvoPronunciation)
--- terms/views.py
+++ terms/views.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import
 
+import requests
+
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.conf import settings
 from django.views.generic import TemplateView, CreateView
 
 from terms import models
+from forvo.models import ForvoWord, ForvoPronunciation
 
 
 class IndexView(TemplateView):
@@ -16,3 +20,39 @@ class TermCreateView(CreateView):
     template_name = 'terms/term_create_form.html'
     fields = ['name']
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        # http://apifree.forvo.com/key/551b8eddc60f432397e3b830b9073e72/format/json/action/word-pronunciations/word/cat/language/en
+        r = requests.get(
+            'http://apifree.forvo.com/key/{}/format/json/action/word-pronunciations/word/{}/language/en'.format(
+                settings.FORVO_API_KEY, form.cleaned_data['name'])
+        )
+        json = r.json()
+
+        if r.status_code == 200:
+
+            word = ForvoWord.objects.create(
+                original_word=form.cleaned_data['name'],
+                num_pronunciations=json['attributes']['total']
+            )
+
+            for item in json['items']:
+                ForvoPronunciation.objects.create(
+                    word=word,
+                    forvo_id=item['id'],
+                    addtime=item['addtime'],
+                    hits=item['hits'],
+                    username=item['username'],
+                    sex=item['sex'],
+                    country=item['country'],
+                    country_code=item['code'],
+                    language_name=item['langname'],
+                    path_mp3=item['pathmp3'],
+                    path_ogg=item['pathogg'],
+                    rate=item['rate'],
+                    num_votes=item['num_votes'],
+                    num_positive_votes=item['num_positive_votes']
+                )
+
+        return HttpResponseRedirect(self.get_success_url())
